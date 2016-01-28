c947bd6bdd62ad1790c2dfd6284797046fabed70
rayanjn <0913881@hr.nl>
2016-01-28 15:26:40 +0100

    Create buildbarak.py

---
+++ buildbarak.py
@@ -0,0 +1,29 @@
+barak = pygame.image.load ('barak')
+while build == True:
+    if event.type == MOUSEBUTTONUP and mousex > ? and mousex < ? and mousey > ? and mousey < ? :
+        while player1soldaat not IsEmpty:
+            templ = player1soldaat.Value
+            if event.type == MOUSEBUTTONUP and mousex > player1soldaat.PositionX and mousex < player1soldaat.PositionX + 42 and mousey> player1soldaat.PositionY and mousey < player1soldaat.PositionY - 42:
+                if event.type == MOUSEBUTTONUP and mousex >player1soldaat.PositionX - 42 and mousex< player1soldaat.PositionX and mousey> player1soldaar.PositionY and mousey < player1soldaat.Position Y - 42:
+                    player1barak = Node(Barak(poop,player1soldaat.PositionX - 42 ,player1soldaat.PositionY),player1barak) #barak links van soldaat
+
+                if event.type == MOUSEBUTTONUP and mousex >player1soldaat.PositionX +42 and mousex< player1soldaat.PositionX +84 and mousey> player1soldaar.PositionY and mousey < player1soldaat.Position Y - 42:
+                    player1barak = Node(Barak(poop,player1soldaat.PositionX + 42 ,player1soldaat.PositionY),player1barak) #barak rechts van soldaat
+
+                if event.type == MOUSEBUTTONUP and mousex >player1soldaat.PositionX and mousex< player1soldaat.PositionX +42 and mousey> player1soldaar.PositionY - 42 and mousey < player1soldaat.Position Y - 84
+                    player1barak = Node(Barak(poop,player1soldaat.PositionX ,player1soldaat.PositionY - 42 ),player1barak) #barak beneden van soldaat
+
+
+                if event.type == MOUSEBUTTONUP and mousex >player1soldaat.PositionX  and mousex< player1soldaat.PositionX +42 and mousey> player1soldaar.PositionY and mousey < player1soldaat.Position Y + 42:
+                    player1barak = Node(Barak(poop,player1soldaat.PositionX ,player1soldaat.PositionY +42),player1barak) #barak links van soldaat
+
+
+
+
+
+
+
+
+
+
+#barak moet naast een soldaat van zichzelf stayen
