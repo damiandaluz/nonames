51280bc50108861f3e4074bc22444f378c948eb4
W1lco <wilco1999muit@gmail.com>
2016-01-28 12:24:15 +0100

    Create Walk.py

---
+++ Walk.py
@@ -0,0 +1,33 @@
+
+
+def VergelijkXP(l)
+    while l.IsEmpty == False:
+        tempL = l.Value
+        if event.type == MOUSEBUTTONUP and mousex > l.PositionX and mousex < (l.PositionX + 42) and mousey > l.PositionY and mousey < (l.PositionY + 42)
+                l.PositionX = l.PositionX + 42
+
+        l.Value = l.Tail
+
+def VergelijkXN(l)
+    while l.IsEmpty == False:
+        tempL = l.Value
+        if event.type == MOUSEBUTTONUP and mousex > l.PositionX and mousex < (l.PositionX + 42) and mousey > l.PositionY and mousey < (l.PositionY + 42)
+                l.PositionX = l.PositionX - 42
+
+        l.Value = l.Tail
+
+def VergelijkYP(l)
+    while l.IsEmpty == False:
+        tempL = l.Value
+        if event.type == MOUSEBUTTONUP and mousex > l.PositionX and mousex < (l.PositionX + 42) and mousey > l.PositionY and mousey < (l.PositionY + 42)
+                l.PositionY = l.PositionY + 42
+
+        l.Value = l.Tail
+
+def VergelijkYN(l)
+    while l.IsEmpty == False:
+        tempL = l.Value
+        if event.type == MOUSEBUTTONUP and mousex > l.PositionX and mousex < (l.PositionX + 42) and mousey > l.PositionY and mousey < (l.PositionY + 42)
+                l.PositionY = l.PositionY - 42
+
+        l.Value = l.Tail
